450c4835e6ab5ae662e6e01f72eb5ba5b759a743
Renu Punjabi <rpunjabi12@gmail.com>
2023-08-01 00:13:17 -0700

    some clean up

--- UIComponents/Buttons/SolidButton1.swift
+++ UIComponents/Buttons/SolidButton1.swift
@@ -8,8 +8,6 @@
 import SwiftUI
 
 struct SolidButton1: View {
-    //    @State var viewModel: SolidButtonViewModel1
-    
     @Binding var title: String
     
     var action: () -> Void
@@ -35,32 +33,6 @@ struct SolidButton1: View {
         .clipShape(RoundedRectangle(cornerRadius: 20))
     }
 }
-
-
-//struct SolidButtonViewModel1 {
-//    var title: String
-//    var icon: String?
-//    var textColor: Color
-//    var bgColor: Color
-//    var size: CGFloat
-//    var isHugging: Bool
-//
-//    init(
-//        title: String,
-//        icon: String? = nil,
-//        textColor: Color,
-//        bgColor: Color,
-//        size: CGFloat = 20,
-//        isHugging: Bool = true
-//    ) {
-//        self.title = title
-//        self.icon = icon
-//        self.textColor = textColor
-//        self.bgColor = bgColor
-//        self.size = size
-//        self.isHugging = isHugging
-//    }
-//}
 
 struct SolidButton1_Previews: PreviewProvider {
     @State static var title = "Solid button 1 title"
